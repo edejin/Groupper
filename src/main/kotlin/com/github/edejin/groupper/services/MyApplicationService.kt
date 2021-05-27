b42efeeed6cf2376c421d5c4059a232f8e39448b
GitHub Action <action@github.com>
2021-05-27 10:02:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/edejin/groupper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/edejin/groupper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.edejin.groupper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.edejin.groupper.MyBundle
 
 class MyApplicationService {
 
